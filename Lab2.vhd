aee5ddd83998c0f4cdf6b236d7285c574cc814e6
Davy MILLION <davym58@live.fr>
2023-01-13 08:55:17 +0100

    [FIX] Removing unecessary top-level input

--- Lab2.vhd
+++ Lab2.vhd
@@ -4,7 +4,6 @@ use ieee.std_logic_1164.all;
 entity Lab2 is
 	 port(
         clock, reset                    : in  std_logic;
-		  ctp_in                          : in  std_logic_vector(3 downto 0);
 		  seg7_out0, seg7_out1, seg7_out2 : out std_logic_vector(6 downto 0)
 	 );
 end entity Lab2;
